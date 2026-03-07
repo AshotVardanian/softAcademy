c6a36900b345658d2224843b53c9365117a324ff
Ashot Vardanian <vardanianashot1648@gmail.com>
2026-03-07 09:44:25 +0400

    Shapes updated

--- softacademy_shapes.hpp
+++ softacademy_shapes.hpp
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <cmath>
 
 namespace softacademy {
 
@@ -16,12 +17,18 @@ public:
 
 class Circle final : public Shape {
 public:
-    explicit Circle(double radius); v
+    explicit Circle(double radius): m_radius(radius){}
 
-    double radius() const noexcept;
+    double radius() const noexcept {
+        return m_radius;
+    }
 
-    double area() const override;
-    double perimeter() const override;
+    double area() const override {
+        return m_radius * m_radius * 3.14;
+    }
+    double perimeter() const override {
+        return 2 * 3.14 * m_radius;
+    }
 
 private:
     double m_radius;
@@ -29,13 +36,21 @@ private:
 
 class Rectangle final : public Shape {
 public:
-    Rectangle(double width, double height);
+    Rectangle(double width, double height) : m_width(width), m_height(height){}
 
-    double width() const noexcept;
-    double height() const noexcept;
+    double width() const noexcept {
+        return m_width;
+    }
+    double height() const noexcept {
+        return m_height;
+    }
 
-    double area() const override;
-    double perimeter() const override;
+    double area() const override {
+        return m_width * m_height;
+    }
+    double perimeter() const override {
+        return 2 * (m_width + m_height);
+    }
 
 private:
     double m_width;
@@ -44,14 +59,29 @@ private:
 
 class Triangle final : public Shape {
 public:
-    Triangle(double a, double b, double c);
+    Triangle(double a, double b, double c){
+        if (valid_triangle(a, b, c)) {
+            m_a = a;
+            m_b = b;
+            m_c = c;
+        }
+    }
 
-    double a() const noexcept;
-    double b() const noexcept;
-    double c() const noexcept;
-
-    double area() const override;
-    double perimeter() const override;
+    double a() const noexcept {
+        return m_a;
+    }
+    double b() const noexcept {
+        return m_b;
+    }
+    double c() const noexcept {
+        return m_c;
+    }
+    double perimeter() const override {
+        return m_a + m_b + m_c;
+    }
+    double area() const override {
+        return sqrt(p/2 * (p/2 - m_a) * (p / 2 - m_b) * (p / 2 - m_c))
+    }
 
 private:
     double m_a;
@@ -59,7 +89,9 @@ private:
     double m_c;
 
 private:
-    bool valid_triangle(double a, double b, double c) const noexcept;
+    bool valid_triangle(double a, double b, double c) const noexcept {
+        return a + b > c && a + c > b && b + c > a;
+    }
 };
 
 } // namespace softacademy
